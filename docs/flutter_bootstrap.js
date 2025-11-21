be22d5fdbbba9edf9c747a0c9d00bd1f81a448ae
Nicolas32800 <nicolas.delzangles@etu.estia.fr>
2025-11-21 01:05:41 +0100

    Build web pour GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3332018189"
+    serviceWorkerVersion: "2600651060"
   }
 });
