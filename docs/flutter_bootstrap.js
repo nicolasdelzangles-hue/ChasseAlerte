4613b9136f060e8b4d1db4225516a992479205c0
Nicolas32800 <nicolas.delzangles@etu.estia.fr>
2025-12-02 22:21:20 +0100

    Update API base URL

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1158189983"
+    serviceWorkerVersion: "3567894238"
   }
 });
